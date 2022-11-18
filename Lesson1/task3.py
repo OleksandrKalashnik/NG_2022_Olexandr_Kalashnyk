61b3366b229447bf19004daa8b7e77b9d600753d
OlexandrKalashnyk <116170085+OleksandrKalashnik@users.noreply.github.com>
2022-11-18 19:31:33 +0200

    Add files via upload

---
+++ Lesson1/task3.py
@@ -0,0 +1,14 @@
+
+currentTime = int(input("Input required number of seconds: "))
+
+seconds = currentTime
+
+minutes = (currentTime / 60)  
+
+hours = (currentTime / 3600 ) 
+
+days = currentTime / 86400
+
+print(f"days:  {int(days)} \n hours: {int(hours)} \n minutes: {int(minutes)} \n seconds: {int(seconds)}")
+input("Press Enter to close program")
+exit()
