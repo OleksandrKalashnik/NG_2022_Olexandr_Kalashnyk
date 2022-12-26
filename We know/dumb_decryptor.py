a3d277cb5bc933fae7931a7e5622e252901f8e70
OlexandrKalashnyk <116170085+OleksandrKalashnik@users.noreply.github.com>
2022-12-27 01:17:30 +0200

    Add files via upload

---
+++ We know/dumb_decryptor.py
@@ -0,0 +1,4 @@
+rot13 = str.maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', 
+   'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm')
+
+print(input("Enter message: ").translate(rot13))
