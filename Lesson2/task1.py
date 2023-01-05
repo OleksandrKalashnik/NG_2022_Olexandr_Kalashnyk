f906c748f9ed1368d7aee273b7255020b8b7875b
OlexandrKalashnyk <116170085+OleksandrKalashnik@users.noreply.github.com>
2023-01-05 10:11:58 +0200

    Delete task1.py

--- Lesson2/task1.py
+++
@@ -1,8 +0,0 @@
-stroka = str(input("Введите строку:"))
-stroka = stroka.lower()
-kolihestvo = {}
-for i in  stroka: 
-    kolihestvo [i] = stroka.count(i)    
-sort = sorted(kolihestvo, key=kolihestvo.get, reverse = True)
-for key in sort:
-    print(str(key), ": ", str(kolihestvo.get(key)))
